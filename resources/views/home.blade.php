a5582511356049ff5276f3e561bc33ebc54d964e
Andrew Shell <andrew@andrewshell.org>
2018-10-31 09:42:23 -0500

    Keeping columns on md screens

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -26,15 +26,15 @@
     @csrf
     <input type="hidden" name="start" value="{{ $start->format('Y-m-d') }}">
     <div class="form-row">
-        <div class="form-group col-lg-1">
+        <div class="form-group col-md-1">
             <label for="time" class="sr-only">Time</label>
             <input type="text" class="form-control text-lg-right" name="time" id="time" placeholder="00:00">
         </div>
-        <div class="form-group col-lg-10">
+        <div class="form-group col-md-10">
             <label for="title" class="sr-only">Title</label>
             <input type="text" class="form-control" name="title" id="title" placeholder="Title">
         </div>
-        <div class="form-group col-lg-1">
+        <div class="form-group col-md-1">
             <div class="btn-group" role="group" aria-label="Basic example">
                 <button type="submit" name="action" value="start" class="btn btn-primary"><font-awesome-icon :icon="['fas', 'play']"></font-awesome-icon></button>
                 <button type="submit" name="action" value="stop" class="btn btn-danger"><font-awesome-icon :icon="['fas', 'stop']"></font-awesome-icon></button>
@@ -45,7 +45,7 @@
 
 @foreach ($entries as $entry)
     <div class="row pt-2 pb-2">
-        <div class="col-lg-11">
+        <div class="col-md-11">
             <button
                 class="btn btn-link entry-toggle-children entry-collapsed"
                 data-children="entry-children-{{ $entry->id }}"
@@ -56,7 +56,7 @@
             <div class="entry-time-duration">{{ $entry->timeDuration() }}</div>
             <div class="entry-title">{{ $entry->task->title }}</div>
         </div>
-        <div class="col-lg-1 text-lg-right">
+        <div class="col-md-1 text-lg-right">
             <form method="post" action="{{ route('home.store') }}">
                 @csrf
                 <input type="hidden" name="parent_id" value="{{ $entry->id }}">
@@ -69,12 +69,12 @@
     <div class="pt-2 pb-2" id="entry-children-{{ $entry->id }}" style="display: none">
     @foreach ($entry->children as $child)
         <div class="row mb-1">
-            <div class="col-lg-10">
+            <div class="col-md-10">
                 <div class="entry-time-padding">&nbsp;</div>
                 <div class="entry-time-duration text-muted">{{ $child->timeDuration() }}</div>
                 <div class="entry-title text-muted">{{ $child->started_at->format('M j, g:i a') }}</div>
             </div>
-            <div class="col-lg-2 text-lg-right">
+            <div class="col-md-2 text-lg-right">
                 <form method="post" action="{{ route('home.destroy') }}">
                     @csrf
                     @method('DELETE')
